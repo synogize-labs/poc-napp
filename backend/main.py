0fd37a220a0130832d1e8ed8fa7cdaedc74774b0
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2025-07-16 12:47:25 +1000

    update backend port to 8001

--- backend/main.py
+++ backend/main.py
@@ -101,4 +101,4 @@ async def health_check_api():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8001) 
